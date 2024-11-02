9640bd6e8da151222cfb4b7e2c65bdbf76f52023
Mike <grabbou@gmail.com>
2024-11-02 20:44:44 +0400

    feat: implement HybridWebSocket on Android (#6)
    * chore: add initial implementation
    
    * revert changes
    
    * chore: stub URL
    
    * chore: remove todo
    
    * chore: add onclosed
    
    * chore: move to const

--- example/App.tsx
+++ example/App.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react'
-import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, Button, Platform, StyleSheet, Text, View } from 'react-native'
 import { WebSocket as FastWebSocket } from 'react-native-fast-ws'
 
 type Result = {
@@ -122,7 +122,7 @@ const testWebsocketMessages = async (opts: {
   incomingTime: number
 }> =>
   new Promise((resolve) => {
-    const inst = new opts.Ws('ws://localhost:3000')
+    const inst = new opts.Ws(WS_URL)
 
     let outgoingTime: number
     let incomingTime: number
@@ -162,6 +162,7 @@ const testWebsocketMessages = async (opts: {
 
 const INCOMING = 10000
 const OUTGOING = 10000
+const WS_URL = Platform.OS === 'android' ? 'ws://10.0.2.2:3000' : 'ws://localhost:3000'
 
 const styles = StyleSheet.create({
   container: {
