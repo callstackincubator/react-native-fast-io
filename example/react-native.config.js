9d4860d443f676c0b46203695610c37700070eba
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-12-02 12:28:54 +0100

    fix: properly link package for dev

---
+++ example/react-native.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const pkg = require('../packages/react-native-fast-io/package.json')
+
+module.exports = {
+  project: {
+    ios: {
+      automaticPodsInstallation: true,
+    },
+  },
+  dependencies: {
+    [pkg.name]: {
+      root: path.join(__dirname, '..', '..', 'packages', 'react-native-fast-io'),
+    },
+  },
+}
